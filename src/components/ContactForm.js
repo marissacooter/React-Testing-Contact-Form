a58ce902cf03b9fe93e4b26cc48303967344095e
Marissa <marissacooterl@gmail.com>
2020-04-09 16:58:32 -0500

    creating last test

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -17,6 +17,7 @@ const ContactForm = () => {
           <label data-testid='firstName' htmlFor="firstName">First Name*</label>
           <input
             name="firstName"
+            id="firstName"
             placeholder="bill"
             ref={register({ required: true, maxLength: 3 })}
           />
@@ -29,6 +30,7 @@ const ContactForm = () => {
           <label data-testid='lastName' htmlFor="lastName">Last Name*</label>
           <input
             name="lastName"
+            id="lastName"
             placeholder="luo"
             ref={register({ required: true })}
           />
@@ -41,14 +43,14 @@ const ContactForm = () => {
           <label data-testid='email' htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name="email" id="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label data-testid='message' htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea name="message" id="message" ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
