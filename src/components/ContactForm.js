169345b1b75c8dd581123b732f62832ee4e252a2
Marissa <marissacooterl@gmail.com>
2020-04-09 15:28:50 -0500

    created and got first test to run and pass

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -14,7 +14,7 @@ const ContactForm = () => {
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label htmlFor="firstName">First Name*</label>
+          <label data-testid='firstName' htmlFor="firstName">First Name*</label>
           <input
             name="firstName"
             placeholder="bill"
@@ -26,7 +26,7 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="lastName">Last Name*</label>
+          <label data-testid='lastName' htmlFor="lastName">Last Name*</label>
           <input
             name="lastName"
             placeholder="luo"
@@ -38,7 +38,7 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
+          <label data-testid='email' htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
           <input name="email" ref={register({ required: true })} />
@@ -47,7 +47,7 @@ const ContactForm = () => {
           )}
         </div>
         <div>
-          <label htmlFor="message">Message</label>
+          <label data-testid='message' htmlFor="message">Message</label>
           <textarea name="message" ref={register({ required: false })} />
         </div>
         {data && (
@@ -55,7 +55,7 @@ const ContactForm = () => {
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input type="submit" id="submit" />
       </form>
     </div>
   );
--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,2 +1,12 @@
 import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
 import ContactForm from './ContactForm';
+
+test('Testing to see if inputs are visible', () => {
+    const {getByTestId} = render (<ContactForm/>)
+    getByTestId('firstName')
+    getByTestId('lastName')
+    getByTestId('email')
+    getByTestId('message')
+})
+
